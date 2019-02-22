7768937c303fcc1c92045e22caab021a62b3bdc8
Derek <wangdere@grinnell.edu>
2019-02-22 20:50:09 +0000

    Finished adding pages

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,4 +1,13 @@
 class PageController < ApplicationController
   def home
   end
+  
+  def parents
+  end
+  
+  def providers
+  end
+  
+  def contacts
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  get 'page/home'
+  #get 'page/home'
+  get 'page/parents' => 'page#parents'
+  get 'page/providers' => 'page#providers'
+  get 'page/contacts' => 'page#contacts'
+  # get  '/home', to: 'static_pages#home'
+  
+  #get  '/parents', to: 'static_pages#parents'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
