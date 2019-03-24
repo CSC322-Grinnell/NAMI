cf043ac6d2cbe1ecbb7a14f67a8ee486057859cc
Yi Qin <qinyi@grinnell.edu>
2019-03-24 00:58:47 +0000

    Modified user model to be compatible with devise (otherwise devise gets argument error)

---
+++ db/migrate/20190324005616_remove_password_digest.rb
@@ -0,0 +1,5 @@
+class RemovePasswordDigest < ActiveRecord::Migration[5.1]
+  def change
+    remove_column :users, :password_digest
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190324005111) do
+ActiveRecord::Schema.define(version: 20190324005616) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,6 @@ ActiveRecord::Schema.define(version: 20190324005111) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
