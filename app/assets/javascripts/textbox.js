96baac408e55c637efcdfec1ce73a25e69c0ba72
Yi Qin <qinyi@grinnell.edu>
2019-03-25 14:35:17 +0000

    Made textbox word count change color when word exceeds 250

--- app/assets/javascripts/textbox.js
+++ app/assets/javascripts/textbox.js
@@ -1,6 +1,7 @@
 // This is the javascript that allows real-time word count for the textbox
 // in "new" view for provider
 document.getElementById('textbox').addEventListener('input', function () {
+        document.getElementById('count').style.color = "black";
         var text = this.value;
         var count;
         if (text != "") {
@@ -10,4 +11,8 @@ document.getElementById('textbox').addEventListener('input', function () {
                 count = 0
         }
         document.getElementById('wordCount').textContent = count;
+        
+        if (count > 250) {
+                document.getElementById('count').style.color = "red";
+        }
     });
