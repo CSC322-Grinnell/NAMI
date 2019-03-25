3d9459ed1703e690740cc6d880c183f58df43390
Derek <wangdere@grinnell.edu>
2019-03-25 15:07:03 +0000

    Small changes to user model to enable dependent destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-    has_one :provider
+    has_one :provider, dependent: :destroy
     validates :password, presence: true, length: { minimum: 6 }
 end
