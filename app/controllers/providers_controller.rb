d673c48a20d0418d1782a1da447e0ab265bc4df7
Yi Qin <qinyi@grinnell.edu>
2019-03-25 13:15:43 +0000

    Modified create action in provider controller to allow database insertion

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -1,9 +1,22 @@
 class ProvidersController < ApplicationController
     def new
-      @provider = Provider.new
     end
     
     def create
-        
+      p.inspect
+      provider_params.inspect
+      p = Provider.new(provider_params)
+      if p.save
+        flash[:success] = "Success!"
+        redirect_to '/page/profile'
+      else
+        render new_provider_path
+      end
+    end
+    
+    private
+
+    def provider_params
+      params.require(:provider).permit(:user_id, :practiceName, :address, :phone, :description)
     end
 end
