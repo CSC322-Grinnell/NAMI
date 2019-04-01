31526b6fd7e32f53dfc67e26275e657b11799158
Yi Qin <qinyi@grinnell.edu>
2019-04-01 20:17:38 +0000

    Added admin authentication

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,4 +1,7 @@
 class AdminsController < ApplicationController
+  
+    include AdminHelper
+    before_action :authenticate_admin!
     
     def grant_admin
       User.find(params[:id]).update_attribute :admin, true
@@ -9,24 +12,14 @@ class AdminsController < ApplicationController
     end
     
     def index
-      if current_user.try(:admin?) # if user is admin
-        @users = User.all
-      else
-        flash[:errors] = ["Only an Admin can view the accounts!"]
-        redirect_to '/providers/profile'
-      end
+      @users = User.order(:admin)
     end
     
     def destroy_user
-      if current_user.try(:admin?)
-        if p = Provider.find_by_user_id(params[:id])
-          p.delete
-        end
-        User.find(params[:id]).delete
-        redirect_to admins_index_path
-      else
-        flash[:errors] = ["Only an Admin can delete the accounts!"]
-        redirect_to '/providers/profile'
+      if p = Provider.find_by_user_id(params[:id])
+        p.delete
       end
+      User.find(params[:id]).delete
+      redirect_to admins_index_path
     end
 end
