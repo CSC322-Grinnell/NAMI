38a848fc583ac731f98d06b1a5e48fbb4c3c90ff
Derek <wangdere@grinnell.edu>
2019-04-08 19:56:29 +0000

    Fixed page CSS

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController < ApplicationController
    
 def search
    if params[:query].present?
-     @results = Provider.search(params[:query])
+     results = Provider.search(params[:query])
      puts @results
    else
      @results = Provider.all
