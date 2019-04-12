e55114a66b7e0000da543b3379a4edd499664c03
Yi Qin <qinyi@grinnell.edu>
2019-04-12 19:27:48 +0000

    Add link and page for advanced search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,14 @@
 class SearchController < ApplicationController
    
-def search
-   if params[:query].present?
-        @results = Provider.search_practiceName(params[:query])
-   # @results = Provider.where("address like ?", "#{params[:query]}%")
-   else
-     @results = Provider.all
+   def search
+      if params[:query].present?
+         @results = Provider.search_practiceName(params[:query])
+      else
+         @results = Provider.all
+      end
    end
-end
+
+   def advanced_search
+   end
 
 end
