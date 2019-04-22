ab78c82e92bb8a944911fa343564ce2cc5239f7a
Yi Qin <qinyi@grinnell.edu>
2019-04-22 20:06:38 +0000

    Added array of inputs for branches in service model

--- app/assets/javascripts/addBranch.js
+++ app/assets/javascripts/addBranch.js
@@ -3,13 +3,25 @@ $("#addBranch").on('click', (e) => {
     var branch = document.createElement("INPUT")
     branch.setAttribute("type", "text")
     branch.setAttribute("class", "branch")
+    branch.setAttribute("multiple", "true")
+    branch.setAttribute("name", "service[branch][]")
     branch.setAttribute("id", "branch[" + index + "]")
     $("#branches")[0].append(branch)
     
     
     var associated_button = document.createElement("span")
     associated_button.setAttribute("class", "delete")
+    associated_button.setAttribute("id", index)
     associated_button.innerHTML = "&#x274C;"
     $("#branches")[0].append(associated_button)
+    
+    $(associated_button).on('click', (e) => {
+        var id = e.target.id
+        var branch_name = "#branch[" + id + "]"
+        console.log(branch_name)
+        $(branch_name).remove();
+        $(this).remove()
+    })
+    
     index++;
 });
