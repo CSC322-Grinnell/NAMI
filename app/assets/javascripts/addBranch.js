4fb2c7557590de220acd941c9a7fdf69e1c872ab
Yi Qin <qinyi@grinnell.edu>
2019-04-22 19:21:21 +0000

    Added a delete sign

--- app/assets/javascripts/addBranch.js
+++ app/assets/javascripts/addBranch.js
@@ -5,4 +5,11 @@ $("#addBranch").on('click', (e) => {
     branch.setAttribute("class", "branch")
     branch.setAttribute("id", "branch[" + index + "]")
     $("#branches")[0].append(branch)
+    
+    
+    var associated_button = document.createElement("span")
+    associated_button.setAttribute("class", "delete")
+    associated_button.innerHTML = "&#x274C;"
+    $("#branches")[0].append(associated_button)
+    index++;
 });
