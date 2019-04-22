2f9b3abf38d53c8248b15ca24841d974cddd1ab4
Yi Qin <qinyi@grinnell.edu>
2019-04-22 19:10:51 +0000

    Changed JS

--- app/assets/javascripts/addBranch.js
+++ app/assets/javascripts/addBranch.js
@@ -1,6 +1,8 @@
+var index = 0
 $("#addBranch").on('click', (e) => {
     var branch = document.createElement("INPUT")
     branch.setAttribute("type", "text")
     branch.setAttribute("class", "branch")
+    branch.setAttribute("id", "branch[" + index + "]")
     $("#branches")[0].append(branch)
 });
