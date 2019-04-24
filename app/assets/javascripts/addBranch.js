d73d7d08996dd5083258789bb2a1dcbe467a4980
Yi Qin <qinyi@grinnell.edu>
2019-04-24 19:48:27 +0000

    Completed the JS for adding a branch in services

--- app/assets/javascripts/addBranch.js
+++ app/assets/javascripts/addBranch.js
@@ -1,13 +1,11 @@
 var index = 0
 $("#addBranch").on('click', (e) => {
+    
     var branch = document.createElement("INPUT")
     branch.setAttribute("type", "text")
     branch.setAttribute("class", "branch")
     branch.setAttribute("multiple", "true")
     branch.setAttribute("name", "service[branch][]")
-    branch.setAttribute("id", "branch[" + index + "]")
-    $("#branches")[0].append(branch)
-    
     
     var associated_button = document.createElement("span")
     associated_button.setAttribute("class", "delete")
@@ -15,12 +13,18 @@ $("#addBranch").on('click', (e) => {
     associated_button.innerHTML = "&#x274C;"
     $("#branches")[0].append(associated_button)
     
+    //container of the input and the delete mark
+    var box = document.createElement("DIV")
+    box.setAttribute("id", "branch[" + index + "]")
+    box.append(branch)
+    box.append(associated_button)
+    $("#branches")[0].append(box)
+    
     $(associated_button).on('click', (e) => {
-        var id = e.target.id
-        var branch_name = "#branch[" + id + "]"
-        console.log(branch_name)
-        $(branch_name).remove();
-        $(this).remove()
+        var id = e.target.id;
+        console.log(id)
+        var branch_name = "#branch\\[" + id + "\\]";
+        $(branch_name).remove()
     })
     
     index++;
