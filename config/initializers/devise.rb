bf0fbb060e6fda883251411dae1a73c3f972ab40
Derek <wangdere@grinnell.edu>
2019-04-26 20:00:29 +0000

    trying to deploy

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -19,7 +19,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
-
+  config.secret_key = 'e6082e36dedb970a65c7a823346b8945ca1c056aff5aba1d9d09c21cae66d069ac9da821f4a61887a7498576c9e83fa25cda6f629d7245802605bb2f368a9151'
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
