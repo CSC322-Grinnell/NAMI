ef0152e793dc527dc3404edf09f70bbda6795177
Yi Qin <qinyi@grinnell.edu>
2019-04-29 19:57:41 +0000

    Fixed user sign_in problem for provider testing

--- test/controllers/providers_controller_test.rb
+++ test/controllers/providers_controller_test.rb
@@ -1,19 +1,40 @@
 require 'test_helper'
 require 'helpers/login.rb'
+
 class ProvidersControllerTest < ActionDispatch::IntegrationTest
     include LoginProcess
+    include Devise::Test::IntegrationHelpers
     
-    test "should get new page" do
-        ApplicationController.any_instance.stubs(:user_signed_in?).returns(true)
-        puts user_signed_in?
-        puts new_provider_path
+    def a_user
+        User.new(email: "qinyi@grinnell.edu", admin: false, encrypted_password: User.new.send(:password_digest, '1234567'))
+    end
+    
+    def an_admin
+        User.new(email: "qinyi2000@outlook.com", admin: true, encrypted_password: User.new.send(:password_digest, '1234567'))
+    end
+    
+    test "should get new page when signed in with user" do
+        sign_in a_user
         get "/providers/new"
         assert_response :success
     end
     
-    test "should get edit page" do
-        ApplicationController.any_instance.stubs(:user_signed_in?).returns(true)
-        get edit_provider_path(Provider.first.id)
+    test "should get edit page when signed in with user" do
+        sign_in a_user
+        get edit_provider_path(Provider.first)
         assert_response :success
+    end
+    
+    test "should create provider when user signed in" do
+        sign_in a_user
+        post '/providers/create', params: { provider: 
+            {practiceName: "1", 
+            address: "1", 
+            phone: "12", 
+            description: "something", 
+            user_id: User.find_by_email("qinyi@grinnell.edu")
+                
+            }}
+        assert_redirected_to '/providers/profile'
     end
 end
