a1b0724df7a81eb8566b01ac18b06fd1a43ebf6f
Yi Qin <qinyi@grinnell.edu>
2019-05-06 19:45:49 +0000

    Added two provider controller tests

--- test/controllers/providers_controller_test.rb
+++ test/controllers/providers_controller_test.rb
@@ -5,14 +5,6 @@ class ProvidersControllerTest < ActionDispatch::IntegrationTest
     include LoginProcess
     include Devise::Test::IntegrationHelpers
     
-    def a_user
-        User.new(email: "qinyi@grinnell.edu", admin: false, encrypted_password: User.new.send(:password_digest, '1234567'))
-    end
-    
-    def an_admin
-        User.new(email: "qinyi2000@outlook.com", admin: true, encrypted_password: User.new.send(:password_digest, '1234567'))
-    end
-    
     test "should get new page when signed in with user" do
         sign_in a_user
         get "/providers/new"
@@ -36,5 +28,37 @@ class ProvidersControllerTest < ActionDispatch::IntegrationTest
                 
             }}
         assert_redirected_to '/providers/profile'
+    end
+    
+    test "should get profile when user signed in" do
+        sign_in a_user
+        get '/providers/profile'
+        assert_response :success
+    end
+    
+    test "should get to edit page when user signed in" do
+        sign_in a_user
+        get edit_provider_path(Provider.first), params: {provider: 
+            {practiceName: "1", 
+            address: "1", 
+            phone: "12", 
+            description: "something", 
+            user_id: User.find_by_email("qinyi@grinnell.edu")
+                
+            }}
+        assert_response :success
+    end
+    
+    test "should update provider when user signed in" do
+        sign_in a_user
+        put provider_path(Provider.first), params: {provider: 
+            {practiceName: "1", 
+            address: "1", 
+            phone: "12", 
+            description: "something", 
+            user_id: User.find_by_email("qinyi@grinnell.edu")
+                
+            }}
+        assert_response :success
     end
 end
