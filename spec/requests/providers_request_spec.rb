5da616be8b6922d041505eb903742dd1f7098ff5
Shelby Frazier <fraziers@grinnell.edu>
2019-10-10 16:43:02 -0500

    Fix how users work for providers spec

--- spec/requests/providers_request_spec.rb
+++ spec/requests/providers_request_spec.rb
@@ -8,18 +8,27 @@ RSpec.configure do |c|
 end
 
 RSpec.describe "Providers Request when signed in with user" do
-  before(:each) { sign_in a_user }
+  before(:each) do
+    @user = a_user
+    sign_in @user
+  end
   it "should get new page" do
     get '/providers/new'
     expect(response).to have_http_status(:success)
   end
+
+  it "should get profile" do
+    get '/providers/profile'
+    expect(response).to have_http_status(:success)
+  end
+
 
   it "should get edit page" do
     get edit_provider_path(Provider.first)
     expect(response).to have_http_status(:success)
   end
 
-  pending "should create provider" do
+  it "should create provider" do
     post providers_create_url,
          params: { provider: {
                      practiceName: "1",
@@ -31,23 +40,6 @@ RSpec.describe "Providers Request when signed in with user" do
     expect(response).to redirect_to(providers_profile_url)
   end
 
-  it "should get profile" do
-    get '/providers/profile'
-    expect(response).to have_http_status(:success)
-  end
-
-  it "should get to edit page" do
-    get edit_provider_url(Provider.first),
-        params: { provider: {
-                    practiceName: "1",
-                    address: "1",
-                    phone: "12",
-                    description: "something",
-                    user_id: User.find_by_email("qinyi@grinnell.edu")
-                  }}
-    expect(response).to have_http_status(:success)
-  end
-
   pending "should update provider" do
     put provider_url(Provider.first),
         params: { provider: {
@@ -55,7 +47,7 @@ RSpec.describe "Providers Request when signed in with user" do
                     address: "1",
                     phone: "12",
                     description: "something",
-                    user_id: User.find_by_email("qinyi@grinnell.edu")
+                    user_id: User.find_by_email("shelby@grinnell.edu")
                   }}
     expect(response).to have_http_status(:success)
   end
