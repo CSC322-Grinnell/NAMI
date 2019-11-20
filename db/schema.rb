a41a3e6895a241c77d0efc615ceb5c43c6cf9158
Shelby Frazier <fraziers@grinnell.edu>
2019-11-20 10:32:22 -0600

    First shot at seeding the database; added Faker gem for sample data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191031222336) do
+ActiveRecord::Schema.define(version: 20191120154222) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,6 +92,7 @@ ActiveRecord::Schema.define(version: 20191031222336) do
   create_table "waivers", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
   add_foreign_key "conditions", "condition_types"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,85 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# coding: utf-8
+
+User.create!(email: "admin@example.com",
+             password: "longpassword",
+             password_confirmation: "longpassword",
+             is_provider: false,
+             is_admin: true)
+
+User.create!(email: "provider@example.com",
+             password: "longpassword",
+             password_confirmation: "longpassword",
+             is_provider: true,
+             is_admin: false)
+
+10.times do |n|
+  User.create!(email: "user#{n}@example.com",
+               password: "longpassword",
+               password_confirmation: "longpassword",
+               is_provider: false,
+               is_admin: false)
+end
+
+ConditionType.create!(name: "Anxiety")
+ConditionType.create!(name: "Mood")
+ConditionType.create!(name: "Developmental")
+ConditionType.create!(name: "Personality")
+ConditionType.create!(name: "Psychotic")
+
+ConditionType.find(1).conditions.create!(name: "Attention Deficit Hyperactivity Disorder (ADHD)",
+                   description: "People with ADHD show a persistent pattern of inattention and/or"\
+                                "hyperactivity-impulsivity that interferes with functioning or development.")
+ConditionType.find(1).conditions.create!(name: "Oppositional Defiant Disorder",
+                   description: "Children and adolescents with ODD may have trouble controlling their temper"\
+                                "and are often disobedient and defiant toward others.")
+ConditionType.find(2).conditions.create!(name: "Autism Spectrum Disorder (ASD)",
+                   description: "Autism spectrum disorder (ASD) is a developmental disorder that makes it"\
+                                "difficult to socialize and communicate with others.")
+ConditionType.find(0).conditions.create!(name: "Generalized Anxiety Disorder (GAD)",
+                   description: "Everyone experiences anxiety sometimes, but when it becomes overwhelming"\
+                                "and repeatedly impacts a person's life, it may be an anxiety disorder.")
+ConditionType.find(1).conditions.create!(name: "Major Depressive Disorder (MDD)",
+                   description: "Depression is more than just feeling sad or going through a rough"\
+                                "patch; it’s a serious mental health condition that requires understanding"\
+                                "and treatment.")
+
+5.times do |n|
+  Insurance.create!(name: Faker::Company.name,
+                    phone: Faker::PhoneNumber.cell_phone,
+                    website: "www.provider#{n}.com")
+end
+
+Waiver.create!(name: "Health and Disability (HD)")
+Waiver.create!(name: "AIDS/HIV (AH)")
+Waiver.create!(name: "Elderly (E)")
+Waiver.create!(name: "Intellectual Disability (ID)")
+Waiver.create!(name: "Brain Injury (BI)")
+
+8.times do |n|
+  service = Services.new(name: "Service #{n}",
+                         description: "This is service #{n}'s description.")
+  (rand(3) + 1).times do
+    condition = Condition.find(rand(5) + 1)
+    services.conditions << condition
+    waiver = Waiver.find(rand(5) + 1)
+    services.waivers << waiver
+  end
+  service.save
+end
+
+20.times do |n|
+  provider = Provider.new(name: Faker::Company.name,
+                          address: Faker::Address.full_address,
+                          phone: Faker::PhoneNumber.cell_phone,
+                          description: "This is provider #{n}",
+                          email: "provider#{n}@example.com")
+  (rand(2) + 1).times do
+    insurance = Insurance.find(rand(5) + 1)
+    provider.insurances << insurance
+  end
+  (rand(5) + 1).times do
+    service = Service.find(rand(8) + 1)
+    provider.services << service
+  end
+  provider.save
+end
