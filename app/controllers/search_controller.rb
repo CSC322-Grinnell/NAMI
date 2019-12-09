46f0358949da0dfb4d0956b4d5f9bca45a9d8560
Shelby Frazier <fraziers@grinnell.edu>
2019-12-08 19:00:06 -0600

    Create some scopes for searching by multiple services, insurances, waivers

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,35 +1,32 @@
 class SearchController < ApplicationController
-   include CheckboxParams
+  include CheckboxParams
 
-   def search
-      if params[:query].present?
-         @results = Provider.search_name(params[:query])
-      else
-         @results = Provider.all
+  def search
+    if params[:query].present?
+      @results = Provider.search_name(params[:query])
+    else
+      @results = Provider.all
+    end
+  end
+
+  def go_to_profile
+    @provider = Provider.find(params[:id])
+  end
+
+  def advanced_search_result
+    name = params[:query]
+    services = params[:service_ids]
+    insurances = params[:insurance_ids]
+    waivers = params[:waiver_ids]
+
+    if ([name, services, insurances, waivers].count{ |q| q.blank? }) == 4
+      @results = Provider.order(name: :asc)
+    else
+      @results = Provider.all
+      unless services.nil?
+        @results = Provider.by_services(services)
       end
-   end
-
-   def go_to_profile
-      @provider = Provider.find(params[:id])
-   end
-
-   def advanced_search
-      @search_criteria = search_criteria
-      @waiver_params = waiver_params
-      @insurance_params = insurance_params
-      @address_params = address_params
-   end
-
-   def advanced_search_result
-      name = params[:query]
-      service = params[:service_options]
-      insurance = params[:insurance_options]
-      waiver = params[:waiver_options]
-
-      if ([name, service, insurance, waiver].count{ |q| q.blank? }) == 4
-        @results = Provider.order(name: :asc)
-      else
-        @results = Provider.search_all([name, service, insurance, waiver])
-      end
-   end
+      @results = @results.search_name(name)
+    end
+  end
 end
